58f6cacab4e6bc828a71efff692f70ab6b9f0c54
Hiren Shah <hirenanandshah@outlook.com>
2021-02-04 10:26:44 -0700

    💡Test for missing homepage in yaml

--- src/__tests__/fromYaml.test.js
+++ src/__tests__/fromYaml.test.js
@@ -43,6 +43,36 @@ pages:
   expect(parsedContent).toMatchSnapshot()
 })
 
+test("Valid navigation yaml format without homepage", () => {
+  const fileContent = `
+type: navigation
+order: 1
+title: About
+name: section-1
+pages:
+  - title: Home
+    url: /
+
+  - title: Page 2
+    url: /page-2/
+    pages:
+      - title: Example
+        url: /example/
+        pages:
+          - title: Nest a page
+            url: /nested/page/
+
+  - title: Diagrams
+    url: /diagrams/
+
+  - title: Hello World
+    url: /hello/
+`
+  const parsedContent = fromYaml(fileContent)
+
+  expect(parsedContent).toMatchSnapshot()
+})
+
 test("Valid yaml but not navigation file", () => {
   const fileContent = `
 name: Yaml file
