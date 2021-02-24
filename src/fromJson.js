d3f01d6305073c3c591738eb023ae3c5ef749182
Hiren Shah <hirenanandshah@outlook.com>
2021-02-24 11:24:02 -0700

    🐛 check for path not empty string

--- src/fromJson.js
+++ src/fromJson.js
@@ -48,7 +48,7 @@ const convertPages = (pages, gitRepoInfo) => {
  */
 const getHomePage = pages => {
   let found = pages.find(page => {
-    return page.path !== undefined
+    return page.path !== undefined && page.path !== ''
   })
 
   if (!found) {
